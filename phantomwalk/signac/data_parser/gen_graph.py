3a3b21b1bcc951b1ec23a3001f6c7fddace48f47
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-03 13:22:10 -0600

    AAAAAAAAAAAAAAAAAAAAA

--- phantomwalk/signac/data_parser/gen_graph.py
+++ phantomwalk/signac/data_parser/gen_graph.py
@@ -27,20 +27,11 @@ args = parser.parse_args()
 print(f"ARGS: {args}")
 
 log = np.genfromtxt(args.log_file, names=True)
-print('0')
 x_axis = log[args.x_axis_key]
-print('1')
 y_axis = log[args.y_axis_key]
-print('2')
-plt.plot(x_axis[args.domain_min:args.domain_max])
-print('3')
-plt.plot(y_axis[args.range_min:args.range_max])
-print('4')
+
+plt.plot(x_axis[args.domain_min:args.domain_max], y_axis[args.range_min:args.range_max])
 plt.xlabel(args.x_axis_name)
-print('5')
 plt.ylabel(args.y_axis_name)
-print('6')
 plt.legend()
-print('7')
 plt.savefig("/bsuhome/cooperpiehl/phantom-graph.png")
-print('8')

3a88d20c384aba3158a3ff5fe46b3860c687c9b6
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-08 10:19:59 -0600

    read out all contents of summary.txt instead of success/failure

--- phantomwalk/signac/dashboard.py
+++ phantomwalk/signac/dashboard.py
@@ -3,25 +3,20 @@
 from signac_dashboard import Dashboard
 from signac_dashboard.modules import StatepointList, DocumentList, ImageViewer, Schema, TextDisplay
 
-def job_failure(job):
-    summary_filename = 'summary.txt' 
+def read_summary(job):
+    filename = 'summary.txt'
     try:
-        with open(job.fn(summary_filename), 'r') as summary_file:
-            summary = summary_file.read().split()
-            if "FAILURE" in summary[0]:
-                return "Job Failed."
-            else:
-                return "Job Succeeded."
+        with open(job.fn(filename, 'r')) as summary_file:
+            return summary_file.read()
     except:
-        return f"Failed to read failure status of {summary_filename}."
+        return f'Failed to read {filename}.'
 
 modules = [
     StatepointList(),
-    DocumentList(context="JobContext"),
     DocumentList(context="ProjectContext"),
     ImageViewer(context="JobContext"),
     ImageViewer(context="ProjectContext"),
-    TextDisplay(message=job_failure),
+    TextDisplay(message=read_summary),
     Schema(),
 ]
 

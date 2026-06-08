25117a0bae0ac2d90d34594b01a1ac1af0a44ad1
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-08 10:16:19 -0600

    add job failure status to signac dashboard

--- phantomwalk/signac/dashboard.py
+++ phantomwalk/signac/dashboard.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 from signac_dashboard import Dashboard
-from signac_dashboard.modules import StatepointList, DocumentList, ImageViewer, Schema
+from signac_dashboard.modules import StatepointList, DocumentList, ImageViewer, Schema, TextDisplay
+
+def job_failure(job):
+    summary_filename = 'summary.txt' 
+    try:
+        with open(job.fn(summary_filename), 'r') as summary_file:
+            summary = summary_file.read().split()
+            if "FAILURE" in summary[0]:
+                return "Job Failed."
+            else:
+                return "Job Succeeded."
+    except:
+        return f"Failed to read failure status of {summary_filename}."
 
 modules = [
     StatepointList(),
@@ -9,6 +21,7 @@ modules = [
     DocumentList(context="ProjectContext"),
     ImageViewer(context="JobContext"),
     ImageViewer(context="ProjectContext"),
+    TextDisplay(message=job_failure),
     Schema(),
 ]
 

1d885f8d605e826d3831dc6301155bdf1892fb68
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-23 10:45:24 -0600

    betterer timeouts & whertnot

--- phantomwalk/signac/compile_graph.py
+++ phantomwalk/signac/compile_graph.py
@@ -3,26 +3,29 @@
 import sys
 import subprocess
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.transforms
+import matplotlib.pyplot as plt
 import signac
 import gsd, gsd.hoomd
 import os
 import math
 
+plt.rcParams.update({'font.size': 14})
+
 # Value is the key used to index into the parameter dictionary
-variables = ["A", "dt"]
+variables = ["k", "gamma"]
 
 constants = {
     "num_pol": 100,
     "num_mon": 100,
     "density": 0.85,
-    "k": 1000,
+    "k": 30000,
     "bond_l": 1.0,
     "r_cut": 1.0,
     "kT": 1.0,
-    "A": 5000,
-    "gamma": 1000,
+    "A": 800,
+    "gamma": 1250,
     "dt": 0.001,
     "seed": 125,
 }
@@ -58,6 +61,8 @@ walltime_errs = []
 NUM_RUNS = 5
 
 for job in jobs:
+    # if job.statepoint["A"] == 30000:
+    #     continue
     xs.append(job.statepoint[variables[0]])
     ys.append(job.statepoint[variables[1]])
 
@@ -70,7 +75,7 @@ for job in jobs:
         walltime_stddev = np.std(job_walltimes)
         walltime_sem = walltime_stddev / math.sqrt(len(job_walltimes))
     else:
-        walltime_mean = 60 * 20 # 20 minute timout
+        walltime_mean = 60 * 10 # 10 minute timout
         walltime_sem = 0
     walltimes = np.append(walltimes, walltime_mean)
     walltime_errs = np.append(walltime_errs, walltime_sem)
@@ -92,17 +97,28 @@ for (x, y, z) in zip(xs, ys, walltimes):
 
 walltime_plot.plot_wireframe(x_grid, y_grid, walltime_grid)
 
-walltime_plot.set(zlabel="Walltime (s)", xlabel=f"{variables[0]}", ylabel=f"{variables[1]}")
+# NORM poster settings
+# walltime_plot.set_zlabel("Walltime (s)", color='white')
+# walltime_plot.set_xlabel(f"{variables[0]}", color='white', labelpad=7)
+# walltime_plot.set_ylabel(f"{variables[1]}", color='white', labelpad=10)
+# walltime_plot.set_yticks(ys)
+# walltime_plot.set_yticklabels(ys, verticalalignment='baseline', horizontalalignment='left')
+# walltime_plot.set_xticks(xs)
+# walltime_plot.set_zticks([4, 5, 6])
+
+# plt.style.use("dark_background")
+# for axis in [walltime_plot.xaxis, walltime_plot.yaxis, walltime_plot.zaxis]:
+#     [t.set_color('white') for t in axis.get_ticklines()]
+#     [t.set_color('white') for t in axis.get_ticklabels()]
 
 walltime_plot.errorbar(xs, ys, walltimes, zerr=walltime_errs, fmt='none', ecolor='r')
 
-#walltime_plot.set_zlim(zmax=5)
+# walltime_plot.set_zlim(zmax=10)
 # walltime_plot.set_zscale('log')
-
-# plt.legend()
+# walltime_plot.set_xlim(xmin=800, xmax=1500)
 
 output_dir = "./time-plots"
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
-# plt.savefig(f"{output_dir}/{variables[0]}-{variables[1]}.png")
-plt.show()
+plt.savefig(f"{output_dir}/{variables[0]}-{variables[1]}.png", dpi=700, transparent=True)
+# plt.show()
